83c140ca9f89f4d5b3c3acd05e00b276db3afb00
nguyentritai96 <nguyentritai96@gmail.com>
2021-10-31 22:56:53 +0700

    add style with import all

---
+++ pages/panel/index.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+
+import basic from 'assets/themes/basic/general.module.css'
+import custom from 'assets/themes/custom/general.module.css'
+
+const listStyles = {
+  basic,
+  custom
+}
+
+const theme = "custom"; // basic || custom
+
+// import(`assets/themes/${theme}/general.module.css`)
+
+const Layout = () => {
+
+  const styles = listStyles[theme]
+
+  return (
+    <>
+      <div className={styles['wrapper']}>
+        <div>You are in {theme} theme</div>
+        <div className={styles['left-content']} onClick={() => alert("Click on left")}>
+          <span>Left Content (Click me)</span>
+        </div>
+        <div className={styles['right-content']}>Right Content</div>
+      </div>
+      <ul>
+        <li>Chỉnh sửa hình dạng, màu sắc div bằng css</li>
+        <li>Chỉnh sửa vị trí div bằng css</li>
+        <li>Chỉnh sửa text div bằng css</li>
+        <li>Action, side effect sẽ được xử lí trong component</li>
+        <li>
+          Thêm mới theme bằng cách add một thư mục chứa các file css được quy
+          định sẵn vào assets/theme
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Layout;
